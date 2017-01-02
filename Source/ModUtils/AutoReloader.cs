e036a59e1b15fc80dd8305ab0b45d58d3743fe68
Kiooeht <kiooeht@gmail.com>
2017-01-02 15:48:44 -0800

    Detect mod dll updates
    (Doesn't do anything about them yet)

---
+++ Source/ModUtils/AutoReloader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace ModUtils
+{
+	public class AutoReloader
+	{
+		private FileSystemWatcher dllWatcher;
+
+		public AutoReloader(string modpath)
+		{
+			dllWatcher = new FileSystemWatcher(modpath, "*.dll");
+			dllWatcher.NotifyFilter = NotifyFilters.LastWrite;
+			dllWatcher.Changed += new FileSystemEventHandler(OnChanged);
+			dllWatcher.EnableRaisingEvents = true;
+
+			var usm = AppMain.gApp.UserScriptManager;
+			foreach (Assembly a in (List<Assembly>)InjectorUtils.GetPrivateMember(usm, "loadedAssemblies")) {
+				Debug.Log(a.GetName().Name);
+			}
+		}
+
+		private void OnChanged(object source, FileSystemEventArgs e)
+		{
+			Debug.Log(e.FullPath);
+			//InjectorUtils.CallPrivateStaticFunc<Assembly>(typeof(UserScriptManager), "LoadAssembly", e.FullPath);
+		}
+	}
+}
